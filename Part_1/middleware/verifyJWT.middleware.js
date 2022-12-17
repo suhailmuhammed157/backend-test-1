cbfe9d7cd33fcf36340f3fb3036fdeea09a7e653
suhailmuhammed157 <suhailmuhammed157@gmail.com>
2022-12-17 20:01:37 +0400

    error handling JWT update

--- Part_1/middleware/verifyJWT.middleware.js
+++ Part_1/middleware/verifyJWT.middleware.js
@@ -6,7 +6,10 @@ const verifyJWT = (req, res, next) => {
   if (!authHeader) return res.sendStatus(401);
   const token = authHeader.split(" ")[1];
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-    if (err) return res.sendStatus(403);
+    if (err)
+      return res.status(400).json({
+        message: "token expired",
+      });
     req.imagePath = decoded.image_path;
     next();
   });
